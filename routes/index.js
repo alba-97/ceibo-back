8433813cad2845b848e0314e8ae1b0c4ed52116d
Ivan Correa <ivan.correa24@yahoo.com>
2023-05-30 16:32:20 -0300

    adding-events-route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const userRouter = require("./users");
-
+const eventsRouter = require("./events");
 const router = express.Router();
 
 router.use("/users", userRouter);
+router.use("/events", eventsRouter);
 
 module.exports = router;
