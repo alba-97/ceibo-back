f96a31e3a83738f2006b165afc711f575da3d01f
alba-97 <alvarezbasilio97@gmail.com>
2023-05-30 17:58:29 -0300

    login, register, y update user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 const express = require("express");
-const { getUsers, addUser, getUser } = require("../controllers/users");
+const {
+  getUsers,
+  addUser,
+  getUser,
+  updateUser,
+  login,
+} = require("../controllers/users");
+
 const router = express.Router();
 
 router.get("/", getUsers);
-router.post("/", addUser);
 router.get("/:id", getUser);
+
+router.post("/register", addUser);
+router.post("/login", login);
+
+router.put("/:id", updateUser);
 
 module.exports = router;
