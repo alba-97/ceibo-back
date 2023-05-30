fdd44571d2b7f6d73758c104c3edced9738ec9cc
alba-97 <alvarezbasilio97@gmail.com>
2023-05-30 12:44:49 -0300

    conexion a mongoatlas

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const routes = require("./routes");
 
+require("dotenv").config();
+app.use(express.json());
+
 app.use("/", routes);
 
 const PORT = 8080;
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((error) => console.error(error));
 
 app.listen(PORT, () => {
   console.log(`Escuchando en puerto ${PORT}`);
