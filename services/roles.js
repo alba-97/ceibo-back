b43b3784bb90184dfaf6f7b639e06b8e0f6e0c7a
Ivan Correa <ivan.correa24@yahoo.com>
2023-06-08 10:25:40 -0300

    finished

--- services/roles.js
+++ services/roles.js
@@ -13,25 +13,3 @@ exports.createNewRole = async (userId, eventId, role) => {
     console.log(error);
   }
 };
-
-/**
- * @swagger
- * components:
- *   schemas:
- *     Roles:
- *       type: object
- *       properties:
- *         user:
- *           type: string
- *           description: associated user id
- *         event:
- *           type: string
- *           description: associated event id
- *         role:
- *           type: string
- *           description: role of the user in the event
- *       example:
- *         user: "647e48221e1b7b3f89c138cc"
- *         event: "647e48261e1b7b3f89c138e9"
- *         role: "Organizador"
- */
