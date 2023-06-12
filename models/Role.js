97f85f230eed84ec7973de1128aaea69781816b6
alba-97 <alvarezbasilio97@gmail.com>
2023-06-12 15:53:43 -0300

    se agrega campo rating al modelo Role

--- models/Role.js
+++ models/Role.js
@@ -4,6 +4,7 @@ const RoleSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   event: { type: mongoose.Schema.Types.ObjectId, ref: "Event" },
   role: { type: String, required: true },
+  rating: { type: Number },
 });
 
 module.exports = mongoose.model("Role", RoleSchema);
