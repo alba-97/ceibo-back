65e82d34332bdb87b1223d17b85c710893d1f5ab
alba-97 <alvarezbasilio97@gmail.com>
2023-06-15 23:22:03 -0300

    ruta para enviar emails con nodemailer

---
+++ mailTransporter.js
@@ -0,0 +1,23 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+let options = {};
+
+try {
+  const domain = process.env.EMAIL_USER.split("@")[1];
+  const service = domain.split(".")[0].toLowerCase();
+  options = {
+    service,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  };
+  res.status(200).send("Se han enviado las invitaciones");
+} catch {
+  console.log("Error en NodeMailer");
+}
+
+const transporter = nodemailer.createTransport(options);
+
+module.exports = transporter;
